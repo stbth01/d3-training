9029d45c635b6f8caa57fb5ebf8310c448570372
Brian Hunscher <bhunscher@mathematica-mpr.com>
2018-02-01 20:39:18 -0500

    renamed fiels

--- src/materials/Week 6/Assignment/cars.js
+++ src/materials/Week 6/Assignment/cars.js
@@ -2,11 +2,8 @@ var fs = require('fs');
 
 const data = fs.readFileSync('./cars.json');
 
-const pdata = fs.readFileSync('./pokemon.json');
-
 let cars = JSON.parse(data.toString());
 
-let pokemon = JSON.parse(pdata.toString());
 
 
 //1
@@ -20,7 +17,7 @@ var numOfCommon = cars.reduce( function(s, d)  {
 
 }, 0);
 
-// console.log("Part 1.1: Number where make is common is "+numOfCommon);
+console.log("Part 1.1: Number where make is common is "+numOfCommon);
 
 //2
 const countries = [];
@@ -42,7 +39,7 @@ cars.forEach( function(d) {
     }
 })
 
-// console.log("Part 1.2: The total number of makes per country is ", countries)
+console.log("Part 1.2: The total number of makes per country is ", countries)
 //3
 const countries_common = [];
 
@@ -77,42 +74,4 @@ cars.forEach( function(d) {
     }
 })
 
-// console.log("Part 1.3: The total number of makes per country is ", countries_common)
-
-//2.1
-let weight_sum = 0;
-let height_sum = 0;
-let n = 0;
-
-pokemon.forEach(function(d) {
-    weight_sum += +d.weight.split(" ")[0] ;
-    height_sum += +d.height.split(" ")[0];
-    n += 1;
-})
-
-let avg_weight_lbs = (weight_sum * 2.20462) /n ;
-let avg_height_inch = (height_sum * 39.3701) /n;
-
-//console.log("Of the " + n + " pokemon in the data set the average weight in lbsis "
-//+ avg_weight_lbs +" and average height in inch is " + avg_height_inch)
-
-
-var egg_total = pokemon.reduce(function(s, d, i) {
-    if(d.weaknesses.find(x => x === "Psychic")) {
-        let e = d.egg.split(" ")[0];
-        if(e === "Not"){
-            return s - 1
-        }
-        else{
-            return s + +e;
-        }
-    }
-    else {
-        return s
-    }
-
-}, 0)
-
-//console.log("total egg distance (value on egg property) for all pokemon who have a weakness of 'Psychic' is "+egg_total)
-
-console.log(pokemon[0].weaknesses.length)
+console.log("Part 1.3: The total number of makes per country is ", countries_common)
